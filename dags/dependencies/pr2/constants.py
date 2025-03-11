fde9305169c713a38b1663d7c5ae56add9a55b64
Jake Peters <jcbptrs@gmail.com>
2025-03-11 13:43:59 -0400

    update TRANSFORM_CONFIG

--- dags/dependencies/pr2/constants.py
+++ dags/dependencies/pr2/constants.py
@@ -2,10 +2,10 @@ import os
 from enum import Enum
 
 # The endpoint for the pr2-transformation Cloud Run.
-PR2_TRANSFORMATION_CLOUD_RUN_URL = "https://ccc-pr2-transformation-dev-1061430463455.us-central1.run.app"
+PR2_TRANSFORMATION_CLOUD_RUN_URL = os.environ.get("PR2_TRANSFORMATION_CLOUD_RUN_URL")
 
 # In production, the project should be set via an environment variable.
-PROJECT = os.environ.get("GCP_PROJECT", "nih-nci-dceg-connect-dev")
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 
 # Datasets for the different stages of your ETL pipeline.
 SOURCE_DATASET = "FlatConnect"
@@ -32,41 +32,38 @@ class StrEnum(str, Enum):
 class SourceTables(StrEnum):
     """Enum for source tables. Uses the SOURCE_DATASET defined globally."""
     _DATASET = SOURCE_DATASET
-    MODULE1_V1 = f"{PROJECT}.{_DATASET}.module1_v1_JP"
-    MODULE1_V2 = f"{PROJECT}.{_DATASET}.module1_v2_JP"
-    MODULE2_V1 = f"{PROJECT}.{_DATASET}.module2_v1_JP"
-    MODULE2_V2 = f"{PROJECT}.{_DATASET}.module2_v2_JP"
-    MODULE3_V1 = f"{PROJECT}.{_DATASET}.module3_v1_JP"
-    MODULE4_V1 = f"{PROJECT}.{_DATASET}.module4_v1_JP"
-    BIOSURVEY_V1 = f"{PROJECT}.{_DATASET}.bioSurvey_v1_JP"
-    CLINICALBIOSURVEY_V1 = f"{PROJECT}.{_DATASET}.clinicalBioSurvey_v1_JP"
-    COVID19SURVEY_V1 = f"{PROJECT}.{_DATASET}.covid19Survey_v1_JP"
-    MENSTRUALSURVEY_V1 = f"{PROJECT}.{_DATASET}.mestrualSurvey_v1_JP"
-    EXPERIENCE2024 = f"{PROJECT}.{_DATASET}.experience2024_JP"
+    MODULE1_V1 = f"{PROJECT_ID}.{_DATASET}.module1_v1_JP"
+    MODULE1_V2 = f"{PROJECT_ID}.{_DATASET}.module1_v2_JP"
+    MODULE2_V1 = f"{PROJECT_ID}.{_DATASET}.module2_v1_JP"
+    MODULE2_V2 = f"{PROJECT_ID}.{_DATASET}.module2_v2_JP"
+    MODULE3_V1 = f"{PROJECT_ID}.{_DATASET}.module3_v1_JP"
+    MODULE4_V1 = f"{PROJECT_ID}.{_DATASET}.module4_v1_JP"
+    BIOSURVEY_V1 = f"{PROJECT_ID}.{_DATASET}.bioSurvey_v1_JP"
+    CLINICALBIOSURVEY_V1 = f"{PROJECT_ID}.{_DATASET}.clinicalBioSurvey_v1_JP"
+    COVID19SURVEY_V1 = f"{PROJECT_ID}.{_DATASET}.covid19Survey_v1_JP"
+    MENSTRUALSURVEY_V1 = f"{PROJECT_ID}.{_DATASET}.mestrualSurvey_v1_JP"
+    EXPERIENCE2024 = f"{PROJECT_ID}.{_DATASET}.experience2024_JP"
 
 class StagingTables(StrEnum):
     """Enum for staging tables. Uses the STAGING_DATASET defined globally."""
     _DATASET = STAGING_DATASET
-    MODULE1_V1_CLEANED_LOOPS = f"{PROJECT}.{_DATASET}.module1_v1_with_cleaned_loop_vars"
-    MODULE1_V2_CLEANED_LOOPS = f"{PROJECT}.{_DATASET}.module1_v2_with_cleaned_loop_vars"
-    MODULE2_V1_CLEANED_LOOPS = f"{PROJECT}.{_DATASET}.module2_v1_with_cleaned_loop_vars"
-    MODULE2_V2_CLEANED_LOOPS = f"{PROJECT}.{_DATASET}.module2_v2_with_cleaned_loop_vars"
-    BIOSURVEY_V1_CLEANED_LOOPS = f"{PROJECT}.{_DATASET}.bioSurvey_v1_JP"
-    CLINICALBIOSURVEY_V1_CLEANED_LOOPS = f"{PROJECT}.{_DATASET}.clinicalBioSurvey_v1_JP"
-    COVID19SURVEY_V1_CLEANED_LOOPS = f"{PROJECT}.{_DATASET}.covid19Survey_v1_JP"
+    MODULE1_V1_CLEANED_LOOPS = f"{PROJECT_ID}.{_DATASET}.module1_v1_with_cleaned_loop_vars"
+    MODULE1_V2_CLEANED_LOOPS = f"{PROJECT_ID}.{_DATASET}.module1_v2_with_cleaned_loop_vars"
+    MODULE2_V1_CLEANED_LOOPS = f"{PROJECT_ID}.{_DATASET}.module2_v1_with_cleaned_loop_vars"
+    MODULE2_V2_CLEANED_LOOPS = f"{PROJECT_ID}.{_DATASET}.module2_v2_with_cleaned_loop_vars"
 
 class CleanTables(StrEnum):
     """Enum for clean tables. Uses the CLEAN_DATASET defined globally."""
     _DATASET = CLEAN_DATASET
-    MODULE1 = f"{PROJECT}.{_DATASET}.module1"
-    MODULE2 = f"{PROJECT}.{_DATASET}.module2"
-    MODULE3 = f"{PROJECT}.{_DATASET}.module3"
-    MODULE4 = f"{PROJECT}.{_DATASET}.module4"
-    BIOSURVEY = f"{PROJECT}.{_DATASET}.bioSurvey"
-    CLINICALBIOSURVEY = f"{PROJECT}.{_DATASET}.clinicalBioSurvey"
-    COVID19SURVEY = f"{PROJECT}.{_DATASET}.covid19Survey"
-    MENSTRUALSURVEY = f"{PROJECT}.{_DATASET}.mestrualSurvey"
-    EXPERIENCE2024 = f"{PROJECT}.{_DATASET}.experience2024"
+    MODULE1 = f"{PROJECT_ID}.{_DATASET}.module1"
+    MODULE2 = f"{PROJECT_ID}.{_DATASET}.module2"
+    MODULE3 = f"{PROJECT_ID}.{_DATASET}.module3"
+    MODULE4 = f"{PROJECT_ID}.{_DATASET}.module4"
+    BIOSURVEY = f"{PROJECT_ID}.{_DATASET}.bioSurvey"
+    CLINICALBIOSURVEY = f"{PROJECT_ID}.{_DATASET}.clinicalBioSurvey"
+    COVID19SURVEY = f"{PROJECT_ID}.{_DATASET}.covid19Survey"
+    MENSTRUALSURVEY = f"{PROJECT_ID}.{_DATASET}.mestrualSurvey"
+    EXPERIENCE2024 = f"{PROJECT_ID}.{_DATASET}.experience2024"
 
 # -----------------------------------------------------------------------------
 # Transformation Configuration
@@ -83,9 +80,9 @@ TRANSFORM_CONFIG = {
             {"source": SourceTables.MODULE1_V2, "destination": StagingTables.MODULE1_V2_CLEANED_LOOPS},
             {"source": SourceTables.MODULE2_V1, "destination": StagingTables.MODULE2_V1_CLEANED_LOOPS},
             {"source": SourceTables.MODULE2_V2, "destination": StagingTables.MODULE2_V2_CLEANED_LOOPS},
-            {"source": SourceTables.BIOSURVEY_V1, "destination": StagingTables.BIOSURVEY_V1_CLEANED_LOOPS},
-            {"source": SourceTables.CLINICALBIOSURVEY_V1, "destination": StagingTables.CLINICALBIOSURVEY_V1_CLEANED_LOOPS},
-            {"source": SourceTables.COVID19SURVEY_V1, "destination": StagingTables.COVID19SURVEY_V1_CLEANED_LOOPS}
+            {"source": SourceTables.BIOSURVEY_V1, "destination": CleanTables.BIOSURVEY},
+            {"source": SourceTables.CLINICALBIOSURVEY_V1, "destination": CleanTables.CLINICALBIOSURVEY},
+            {"source": SourceTables.COVID19SURVEY_V1, "destination": CleanTables.COVID19SURVEY}
         ]
     },
     "merge_table_versions": {
