fa07b1d93dd65d2e6a255d23f67cb93bc98d2965
Jake Peters <jcbptrs@gmail.com>
2025-03-20 10:47:03 -0400

    update constants

--- bin/push_dags.sh
+++ bin/push_dags.sh
@@ -4,24 +4,21 @@
 set -e
 
 # Configure for dev environment
-<<<<<<< HEAD
-PROJECT_ID="nih-nci-dceg-connect-stg-5519"
-GCS_DAGS_FOLDER="gs://us-central1-ccc-jp-stg-c97d7d50-bucket/dags/"
+
+# PROJECT_ID="nih-nci-dceg-connect-stg-5519"
+# GCS_DAGS_FOLDER="gs://us-central1-ccc-jp-stg-c97d7d50-bucket/dags/"
+
 # PROJECT_ID="nih-nci-dceg-connect-dev"
 # GCS_DAGS_FOLDER="gs://us-central1-ccc-jp-dev-54b9b374-bucket/dags/"
-=======
-PROJECT_ID="nih-nci-dceg-connect-dev"
->>>>>>> main
+
+PROJECT_ID="nih-nci-dceg-connect-prod-6d04"
+GCS_DAGS_FOLDER="gs://us-central1-ccc-orchestrato-a82b22b0-bucket/dags/"
 
 echo "Setting GCP project to: $PROJECT_ID"
 gcloud config set project "$PROJECT_ID"
 
 echo "Copying local DAGs to dev Composer bucket..."
-<<<<<<< HEAD
 gsutil -m cp -r dags/* $GCS_DAGS_FOLDER
-=======
-gsutil -m cp -r dags/* gs://"$COMPOSER_BUCKET"/dags/
->>>>>>> main
 
 echo "Done. DAGs have been pushed to dev environment!"
  
